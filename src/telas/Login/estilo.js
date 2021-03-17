0b3c0cb7b00837b01799e3ac8d36043950209d20
juomzinho <joaopaulocs09@gmail.com>
2021-03-17 01:59:50 -0300

    tela de login quase pronta

--- src/telas/Login/estilo.js
+++ src/telas/Login/estilo.js
@@ -7,4 +7,54 @@ export const Container = styled.SafeAreaView`
     align-items: center;
     background-color: #211F1C;
     color: #DCDCDC;
+    width: 100%
 `;
+
+export const InputArea = styled.View`
+    width: 100%
+    justify-content: center
+    align-items: center
+`
+
+export const CustomButton = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    height: 50;
+    background-color: #B81F00;
+    borderRadius: 5;
+    margin-bottom: 30;
+`
+
+export const LoginInput = styled.View`
+    border: 1px solid #dcdcdc
+    align-items: center
+    width: 80%
+    borderRadius : 5
+    margin-bottom: 30
+    padding-left: 15px
+    flex-direction: row
+    height: 50px
+`
+
+export const LoginText = styled.TextInput`
+    color: #acacac;
+    margin-left: 10px
+    width: 70%
+`
+
+export const CustomButtonText = styled.Text`
+    color: #dcdcdc
+`
+export const Cadastro = styled.TouchableOpacity`
+    flex-direction: row
+    margin-bottom: 15
+`
+
+export const CadastroText = styled.Text`
+    color: #dcdcdc
+`
+
+export const CadastroButton = styled.Text`
+    color: #B81F00
+`
--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, TextInput, View } from 'react-native'
-import { Container } from './estilo'
+import { 
+    Container,
+    InputArea,
+    LoginInput,
+    CustomButton,
+    CustomButtonText,
+    LoginText,
+    Cadastro,
+    CadastroText,
+    CadastroButton
+} from './estilo'
+import Logo from '../../assets/Logo.svg'
+import User from '../../assets/icons/user.svg'
+import Cadeado from '../../assets/icons/locked.svg'
+import Olho from '../../assets/icons/view.svg'
 
 export default () => {
+
+    const [recebeUser, setUser] = useState('')
+    const [recebeSenha, setSenha] = useState('')
+
+
     return(
         <Container>
-            <View>
-                <Text>Usuário</Text>
-                <TextInput name="user"></TextInput>
+
+            <View style={{marginBottom: 30}}>
+                <Logo height="120" width="120"/>
             </View>
+
+            <InputArea>
+                <LoginInput value={recebeUser} onChangeText={t=>setUser(t)} >
+                    <User height="24" width="24" fill="#acacac" />
+                    <LoginText placeholder="Usuário" />
+                </LoginInput>
+
+                <LoginInput value={recebeSenha} onChangeText={t=>setUser(t)} >
+                    <Cadeado height="24" width="24" fill="#acacac" />
+                    <LoginText placeholder="Senha" secureTextEntry={true}/>
+                    <Olho height="24" width="24" fill="#acacac" />
+                </LoginInput>
+
+                <CustomButton>
+                    <CustomButtonText>Entrar</CustomButtonText>
+                </CustomButton>
+
+                <Cadastro>
+                    <CadastroText>Ainda não é cadastrado? </CadastroText>
+                    <CadastroButton>Cadastre-se</CadastroButton>
+                </Cadastro>
+
+                <Cadastro>
+                    <CadastroButton>Recuperar Senha</CadastroButton>
+                </Cadastro>
+
+            </InputArea>
         </Container>
     )
 
