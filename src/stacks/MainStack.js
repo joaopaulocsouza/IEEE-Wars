205f142000ca750fd14846290dc0aed570bd8496
juomzinho <joaopaulocs09@gmail.com>
2021-03-19 16:43:38 -0300

    config main stack

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -4,13 +4,13 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Carregamento from '../telas/Carregamento'
 import Login from '../telas/Login'
 import Cadastro from '../telas/Cadastro'
-import Inicio from '../telas/Inicio'
+import Pics from '../telas/Pics'
 import Recuperar from '../telas/Recuperar'
 import mensagemSucessoRecuperar from '../telas/Recuperar/mensagemSucesso'
 import mensagemErroRecuperar from '../telas/Recuperar/mensagemErro'
-import { useNavigation } from '@react-navigation/core'
-import { Button } from 'react-native'
-import Seta from '../assets/icons/seta.svg'
+import NavBottom from '../components/NavBottom'
+import RankingIndividual from '../telas/Ranking/RankingIndividual'
+import RankingProjetos from '../telas/Ranking/RankingProjeto'
 
 const Stack = createStackNavigator();
 
@@ -23,9 +23,12 @@ export default () => (
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Recuperar" component={Recuperar} />
-        <Stack.Screen name="Inicio" component={Inicio} />
         <Stack.Screen name="mensagemSucessoRecuperar" component={mensagemSucessoRecuperar} />
         <Stack.Screen name="mensagemErroRecuperar" component={mensagemErroRecuperar} />
+        <Stack.Screen name="NavBottom" component={NavBottom} />
+        <Stack.Screen name="Pics" component={Pics} />
+        <Stack.Screen name="RankingIndividual" component={RankingIndividual} />
+        <Stack.Screen name="RankingProjetos" component={RankingProjetos} />
     </Stack.Navigator>
     
 )
